f542d1e788a09c4a346ad29024d02fbfd41350b7
777shuang <shuang777@outlook.jp>
2024-08-11 11:35:01 +0900

    `.docx`ファイルを読み取ってテンプレートに流し込む

--- src/main.nim
+++ src/main.nim
@@ -1,5 +1,37 @@
-# This is just an example to get you started. A typical binary package
-# uses this file as the main entry point of the application.
+import std/os
+import std/enumerate
+import std/[xmltree, xmlparser]
+import zippy/ziparchives
+import nimja/parser
+import types, utils
 
-when isMainModule:
-  echo("Hello, World!")
+proc render(textboxes: seq[TextBox]): string =
+  compileTemplateFile(getScriptDir() / "template.nimja")
+
+let reader = openZipArchive("test.docx")
+let xmlnode = parseXml(reader.extractFile("word/document.xml"))
+reader.close()
+
+var textboxes: seq[TextBox] = @[]
+
+for paragraph in xmlnode.child("w:body"):
+  for (i, run) in enumerate(paragraph):
+    let drawing = run.child("w:drawing")
+    if drawing != nil:
+
+      let anchor = drawing.child("wp:anchor")
+      if anchor != nil:
+        let x = anchor.child("wp:positionH").child("wp:posOffset").innerText
+        let y = anchor.child("wp:positionV").child("wp:posOffset").innerText
+        let w = anchor.child("wp:extent").attr("cx")
+        let h = anchor.child("wp:extent").attr("cy")
+
+        var textbox: TextBox
+        textbox.x = emu2mm($x)
+        textbox.y = emu2mm($y)
+        textbox.w = emu2mm($w)
+        textbox.h = emu2mm($h)
+        textbox.text = "みかん"
+        textboxes.add(textbox)
+
+echo render(textboxes)
